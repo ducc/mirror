629462d6195c2520136897f5789ee7c5da03c30f
Joe Burnard <spongybacon@hotmail.co.uk>
2024-10-27 16:26:20 +0000

    multiple channels

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 ```
-export TOKEN="...."
-export CHANNEL_ID=""
-export WEBHOOK_URL="https://discord.com/api/webhooks/..."
+export TOKEN=""
+export WEBHOOK_URL=""
+
+// Guild to mirror messages from
+export SOURCE_GUILD_ID=""
+
+// Channel to mirror messages from. Mirrors all channels if SOURCE_CHANNEL_ID is empty.
+export SOURCE_CHANNEL_ID=""
+
+// Channel to mirror messages into
+export TARGET_CHANNEL_ID=""
 
 go run main.go
 ```
--- main.go
+++ main.go
@@ -12,12 +12,36 @@ import (
 )
 
 var (
-	token           = os.Getenv("TOKEN")
-	sourceChannelId = os.Getenv("CHANNEL_ID")
-	webhookUrl      = os.Getenv("WEBHOOK_URL")
+	token      = os.Getenv("TOKEN")
+	webhookUrl = os.Getenv("WEBHOOK_URL")
+
+	// Guild to mirror messages from
+	sourceGuildId = os.Getenv("SOURCE_GUILD_ID")
+	// Channel to mirror messages from. Mirrors all channels if SOURCE_CHANNEL_ID is empty.
+	sourceChannelId = os.Getenv("SOURCE_CHANNEL_ID")
+	// Channel to mirror messages into
+	targetChannelId = os.Getenv("TARGET_CHANNEL_ID")
+)
+
+var (
+	// mapping from channel id to channel name
+	channelNameCache = make(map[string]string)
 )
 
 func main() {
+	if token == "" {
+		panic("TOKEN is required")
+	}
+	if webhookUrl == "" {
+		panic("WEBHOOK_URL is required")
+	}
+	if sourceGuildId == "" {
+		panic("SOURCE_GUILD_ID is required")
+	}
+	if targetChannelId == "" {
+		panic("TARGET_CHANNEL_ID is required")
+	}
+
 	discord, err := discordgo.New(token)
 	if err != nil {
 		panic(err)
@@ -37,15 +61,44 @@ func main() {
 	<-c
 }
 
+func getChannelName(s *discordgo.Session, channelID string) (string, error) {
+	if cachedName := channelNameCache[channelID]; cachedName != "" {
+		return cachedName, nil
+	}
+
+	channel, err := s.Channel(channelID)
+	if err != nil {
+		return "", fmt.Errorf("getting channel %s: %w", channelID, err)
+	}
+
+	return channel.Name, nil
+}
+
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.ChannelID != sourceChannelId {
+	// ignore messages in target channel
+	if m.ChannelID == targetChannelId {
 		return
+	}
+
+	// ignore messages outside of source guild
+	if sourceGuildId != m.GuildID {
+		return
+	}
+
+	// if SOURCE_CHANNEL_ID is set then ignore messages from other channels
+	if sourceChannelId != "" && m.ChannelID != sourceChannelId {
+		return
+	}
+
+	channelName, err := getChannelName(s, m.ChannelID)
+	if err != nil {
+		panic(err)
 	}
 
 	params := discordgo.WebhookParams{
 		Content:         m.Message.Content,
 		Components:      m.Message.Components,
-		Username:        m.Author.Username,
+		Username:        truncate(fmt.Sprintf("%s - #%s", m.Author.Username, channelName), 80),
 		AvatarURL:       m.Author.AvatarURL(""),
 		Embeds:          m.Message.Embeds,
 		Attachments:     m.Message.Attachments,
@@ -71,4 +124,12 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 		fmt.Println(string(respBody))
 	}
+}
+
+func truncate(inp string, maxLength int) string {
+	if len(inp) <= maxLength {
+		return inp
+	}
+
+	return inp[0:maxLength]
 }
